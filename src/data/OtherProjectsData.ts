bba6bedaaa73d8e8085a13dfa39b0689481e4892
PauFusco <99730051+PauFusco@users.noreply.github.com>
2025-09-02 20:42:43 +0200

    Update OtherProjectsData.ts

--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -3,12 +3,12 @@ import ProjectData from '@/data/ProjectData.ts'
 export default [
   new ProjectData("project-6", "Primal Cycle", "img/projects/primalcycle.png", `
     <div class="paragraph">
-      Advance through rooms, defeat the boss, break the cycle. A rogue-lite 2-stick arcade shooter:
+      <strong>Advance</strong> through rooms, <strong>defeat</strong> the boss, <strong>break</strong> the cycle. A rogue-lite 2-stick arcade shooter:
       face enemies to become strong enough to defeat the final boss: the mighty Hippopotamus.
       <br/>
-      The entry to the GMTK 2025 game jam made by Friendly Servers, a team of 7 people.
+      The entry to the GMTK 2025 game jam made by Friendly Servers, a team of 7 people who just graduated of their Game Design and Development degree.
       <br/>
-      Done with Unity in 4 days, we went from a simple idea to the final game.
+      Done in Unity in four days, we went from the base theme of the game jam, "cycle", to the final game.
     </div>
 
     <div class="paragraph">
@@ -31,14 +31,19 @@ export default [
     <div class="paragraph center">
       <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzc2NjYzOS8yMjU0MzE5MC5wbmc=/original/nwM%2BDr.png" alt="Primal Cycle Screenshot" />
       <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzc2NjYzOS8yMjU0MzI1MS5wbmc=/original/Iqdol2.png" alt="Sultans Of Sing Screenshot" />
-      <!--<img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> -->
     </div>
     `, "#c14b06", false, true),
 
   new ProjectData("project-6", "Morpheus' Dream", "img/projects/morpheusdream.png", `
     <div class="paragraph">
-      <strong>Drew Blood</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-      <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@karolina-grabowska">Karolina Grabowska</a>.
+      <strong>You wake up, do your chores, go to sleep, wake up, do your ch... wait, is that pizza floating?</strong>
+      <br/>
+      A short mystery walking sim in which you spend your day doing your chores to go to sleep, and then wake up.
+      Your apartment becomes more and more distorted as your pet fish, Morpheus, observes diligently.
+      <br/>
+      The entry to the 7a Gran CITM Game Jam made by Los Juanes, a group of 3 people.
+      <br/>
+      Done with Unity in half a week, we took the main theme of the jam, "dreams and nightmares", and created this game.
     </div>
     
     <div class="paragraph center">
@@ -48,82 +53,95 @@ export default [
     <div class="paragraph">
       Main features :
       <ul>
-      <li>Some stuff</li>
-      <li>Some great stuff</li>
-      <li>More awesome stuff</li>
-      <li>And then some</li>
+      <li>Mission System</li>
+      <li>Iteractable Elements</li>
+      <li>Color Aberration Shaders</li>
       </ul>
     </div>
 
     <div class="paragraph">
     <div class="notice">
-      Windows build available in <a href="https://softdrawss.itch.io/morpheus-dream" target="_blank">itch.io</a>
+      Windows build available in <a href="https://softdrawss.itch.io/morpheus-dream" target="_blank">itch.io</a>.
     </div>
     </div>
 
     <div class="paragraph center">
-      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjUwNjQxNy8xNDg5MjY5OC5wbmc=/original/xWjKug.png" alt="Morpheus' Dream Screenshot" />
-      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjUwNjQxNy8xNDg5MjY2NS5wbmc=/original/Dp2tnw.png" alt="Morpheus' Dream Screenshot" />
-      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjUwNjQxNy8xNDg5MjgwMy5wbmc=/original/aabr%2FF.png" alt="Morpheus' Dream Screenshot" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjUwNjQxNy8xNDg5MjY5OC5wbmc=/original/xWjKug.png" alt="Morpheus' Dream Screenshot 1" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjUwNjQxNy8xNDg5MjY2NS5wbmc=/original/Dp2tnw.png" alt="Morpheus' Dream Screenshot 2" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjUwNjQxNy8xNDg5MjgwMy5wbmc=/original/aabr%2FF.png" alt="Morpheus' Dream Screenshot 3" />
     </div>
     `, "#a58c6c"),
 
   new ProjectData("project-8", "No Time to Di(c)e", "img/projects/notimetodice.png", `
     <div class="paragraph">
-      <strong>The Art of Walking</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-    </div>
-
-    <div class="paragraph center">
-      <a href="https://play.google.com/store/apps/details?id=someapp" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+      <strong>Fight through the casino, seek revenge against the deck of cards, who have wronged you.</strong>
+      <br/>
+      The dice lived in peace in the casino with the cards, until one day, in an unfortunate turn of events, 
+      they are separated. In that moment, our protagonist decides to take justice into their own hands.
+      <br/>
+      In this 2-stick arcade shooter you incarnate a dice, using the power of chance to empower themselves and defeat the cards.
+      <br/>
+      An entry to the GMTK 2022 Game Jam, made by a group of 12 first-year Game Design and Development students, using SDL2 and C++.
     </div>
 
     <div class="paragraph">
       Main features :
       <ul>
-      <li>Some stuff</li>
-      <li>Some great stuff</li>
-      <li>More awesome stuff</li>
-      <li>And then some</li>
+      <li>Menu Music</li>
+      <li>All Sound Effects</li>
+      <li>Sound Implementation</li>
       </ul>
     </div>
 
     <div class="paragraph">
       <div class="notice">
-        Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        Windows build available in <a href="https://xgauss05.itch.io/no-time-to-dice" target="_blank">itch.io</a>.
       </div>
     </div>
 
     <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
+        <img class="phone-screenshot" src="https://img.itch.zone/aW1hZ2UvMTYyMjY1OS85NTc0NDUwLmpwZw==/original/rO2oRF.jpg" alt="No Time to Di(c)e Screenshot 1" />
+        <img class="phone-screenshot" src="https://img.itch.zone/aW1hZ2UvMTYyMjY1OS85NTc0NDU0LmpwZw==/original/OlOJpO.jpg" alt="No Time to Di(c)e Screenshot 2" />
+        <img class="phone-screenshot" src="https://img.itch.zone/aW1hZ2UvMTYyMjY1OS85NTc0NDYxLmpwZw==/original/TAAIud.jpg" alt="No Time to Di(c)e Screenshot 3" />
+        <img class="phone-screenshot" src="https://img.itch.zone/aW1hZ2UvMTYyMjY1OS85NTc0NDcxLmpwZw==/original/%2FC%2Fyen.jpg" alt="No Time to Di(c)e Screenshot 4" />
     </div>
     `, "#16551e"),
 
   new ProjectData("project-7", "In the Tracks of Fate", "img/projects/inthetracksoffate.png", `
     <div class="paragraph">
-      <strong>Sultans Of Sing</strong> is a thing of beauty that I am so proud of. It's available on Windows Store because why not.
+      <strong>Interact with the other passangers of this train day after day... or is this day repeating?</strong>
       <br/>
-      Image by <a href="https://www.pexels.com/fr-fr/@aldoalz" target="blank">Aldo Picaso</a>.
+      Ask until you are satisfied, uncover the misteries of the train and its passangers, and also about yourself.
+      <br/>
+      A point&click adventure that focuses on character interaction and relationship building through dialog and observation.
+      <br/>
+      The entry of the 8a Gran CITM Game Jam of Little Bat Productions, a group of 5 people in their last year of
+      Game Design and Development, made with Unity.
+      <br/>
+      This game won the "Premi F5 / Dones Tech" award, which focused on accessibility and minority representation.
     </div>
     
     <div class="paragraph center">
-      <a href="https://www.microsoft.com/en-us/p/some-app" target="_blank"><img src="img/projects/ms-store-logo.png" alt="MS Store button" /></a>
+        <iframe class="youtube" src="https://www.youtube.com/embed/szdzXoK7Ll0" frameborder="0" allowfullscreen></iframe>
     </div>
 
     <div class="paragraph">
       Main features :
       <ul>
-      <li>Some stuff</li>
-      <li>Some great stuff</li>
-      <li>More awesome stuff</li>
-      <li>And then some</li>
+      <li>Player Movement</li>
+      <li>Interactions, including items and doors</li>
+      <li>Builded Map and its Colliders</li>
+      <li>Original Soundtrack</li>
+      <li>Unused Inventory System</li>
       </ul>
     </div>
 
     <div class="paragraph center">
-      <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-      <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-      <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzI4NTQyOC8xOTYxNDM5MS5wbmc=/original/WQtDAX.png" alt="In the Tracks of Fate Screenshot 1" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzI4NTQyOC8xOTYxNDQwMS5wbmc=/original/26Woa0.png" alt="In the Tracks of Fate Screenshot 2" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzI4NTQyOC8xOTYxNDQzMi5wbmc=/original/sg8TZv.png" alt="In the Tracks of Fate Screenshot 3" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzI4NTQyOC8xOTY2Mzg5OS5wbmc=/original/TJHBhc.png" alt="In the Tracks of Fate Screenshot 4" />
+      <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzI4NTQyOC8xOTY2Mzk0MC5wbmc=/original/bCsrHh.png" alt="In the Tracks of Fate Screenshot 5" />
     </div>
     `, "#5621e9", false, true),
 ];
